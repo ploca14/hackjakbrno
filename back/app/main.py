db8b08220aab1a88abab0d79bfc07ab681cf399d
Hugo Mrázek <hugo.mrazek@gmail.com>
2025-11-29 15:38:27 +0100

    Added wildcard expose_headers

--- back/app/main.py
+++ back/app/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 @app.on_event("startup")
